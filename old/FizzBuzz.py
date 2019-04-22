b11b862bb8d03272a4712fcc49f613171b649630
SuperConsole <superconsoledjem@gmail.com>
2019-04-22 13:15:27 +0900

    second commit

---
+++ old/FizzBuzz.py
@@ -0,0 +1,16 @@
+def FizzBuzz(num, maxNum):
+    if num%15  ==  0:
+        print(str(num) + " FizzBuzz")
+    elif num%3 ==  0:
+        print(str(num) + " Fizz")
+    elif num%5 ==  0:
+        print(str(num) + " Buzz")
+    else:
+        print(str(num))
+    
+    if num <   maxNum:
+        FizzBuzz(num+1, maxNum)
+
+if __name__ == "__main__":
+    iter = [1300, 1350]
+    FizzBuzz(iter[0], iter[-1])
---
+++ old/KaomojiMaker.py
@@ -0,0 +1,5 @@
+def KaomojiMaker(prtEye, prtLip):
+    print("("+str(prtEye)+str(prtLip)+str(prtEye)+")/")
+
+if __name__ == "__main__":
+    KaomojiMaker("1", "+")
